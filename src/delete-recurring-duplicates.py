928724425effcfad85ada6a920de32e59f4b8b00
Ely Deckers <e.deckers@gmail.com>
2022-02-01 16:02:39 +0100

    Add MPL-2.0 headers to each script

--- src/delete-recurring-duplicates.py
+++ src/delete-recurring-duplicates.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Ely Deckers.
+#
+# This source code is licensed under the MPL-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 from datetime import datetime
 import os.path
 
